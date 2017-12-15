dbbe0db8f82dd4c0962b26a987faa2d4bfade9c9
pauliantonia <paulinaantonia.94@gmail.com>
2017-12-15 18:15:21 -0300

    evaluación con creación de whats app web

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready( {
+});
+
+/*cuando se aprete el boton glyphicon-menu-right o se aprete la tecla ENTER, solo si es que hay texto en el input.
+se envie el texto ingresado en un div vacio que estara en la seccion CONVERSATIONS*/
+var newDiv = $('.newDiv')
+
+$('.container-fluid').append(newDiv) 
+    ;
